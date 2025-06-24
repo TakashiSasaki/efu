52db10fa843b2c2aee8afac40ece1a91f8dc0cf2
Takashi SASAKI <takashi316@gmail.com>
2025-06-25 03:19:49 +0900

    Add unit test for write_efu using docs CSV example

---
+++ tests/test_write_efu.py
@@ -0,0 +1,47 @@
+import pathlib
+import sys
+
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1] / 'src'))
+
+from efu_csv_utils import write_efu
+
+
+def test_write_efu_simple(tmp_path):
+    header = [
+        "Filename",
+        "Size",
+        "Date Modified",
+        "Date Created",
+        "Attributes",
+    ]
+    rows = [
+        ["C:\\msys64", "", "133876022280081366", "133739602603410395", "16"],
+        [
+            "C:\\msys64\\autorebase.bat",
+            "82",
+            "133262362720000000",
+            "133739602600000000",
+            "32",
+        ],
+        [
+            "C:\\msys64\\clang64",
+            "0",
+            "133665511886007850",
+            "133665511886007850",
+            "16",
+        ],
+    ]
+
+    out_file = tmp_path / "out.efu"
+    write_efu(rows, header, str(out_file), newline="\r\n")
+
+    expected = (
+        "Filename,Size,Date Modified,Date Created,Attributes\r\n"
+        "\"C:\\msys64\",,133876022280081366,133739602603410395,16\r\n"
+        "\"C:\\msys64\\autorebase.bat\",82,133262362720000000,133739602600000000,32\r\n"
+        "\"C:\\msys64\\clang64\",0,133665511886007850,133665511886007850,16\r\n"
+    )
+
+    with open(out_file, "r", newline="") as f:
+        content = f.read()
+    assert content == expected
