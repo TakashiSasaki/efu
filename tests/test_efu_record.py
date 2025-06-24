ba6abaeb22d3d8b3a9480fc3a28436965a056666
Takashi SASAKI <takashi316@gmail.com>
2025-06-25 08:17:42 +0900

    Fix tests to import efu module

---
+++ tests/test_efu_record.py
@@ -0,0 +1,47 @@
+import pathlib
+import sys
+
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1] / 'src'))
+
+from efu import EfuRecord
+
+
+def test_efu_record_initialization():
+    header = [
+        "Filename",
+        "Size",
+        "Date Modified",
+        "Date Created",
+        "Attributes",
+    ]
+    rec = EfuRecord(header)
+    assert list(rec.keys()) == header
+    assert all(value is None for value in rec.values())
+
+
+def test_efu_record_with_data():
+    header = [
+        "Filename",
+        "Size",
+        "Date Modified",
+        "Date Created",
+        "Attributes",
+    ]
+    rec = EfuRecord(header, {"Filename": "foo"}, Size=100)
+    assert rec["Filename"] == "foo"
+    assert rec["Size"] == 100
+    for key in header:
+        assert key in rec
+
+
+def test_efu_record_attributes():
+    header = ["Filename"]
+    rec = EfuRecord(header)
+    assert rec.last_seen == 0
+    assert rec.first_seen == 0
+    assert rec.last_lost == 0
+
+    rec2 = EfuRecord(header, last_seen=1, first_seen=2, last_lost=3)
+    assert rec2.last_seen == 1
+    assert rec2.first_seen == 2
+    assert rec2.last_lost == 3
